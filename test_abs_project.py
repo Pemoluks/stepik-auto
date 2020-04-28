b309881a3f635a49237ae2d10a0965ba2907be0b
Pemoluks <61008543+Pemoluks@users.noreply.github.com>
2020-04-28 19:37:10 +0300

    используем unittest

--- test_abs_project.py
+++ test_abs_project.py
@@ -1,10 +1,14 @@
-def test_abs1():
-    assert abs(-42) == 42, "Should be absolute value of a number" 
-
-def test_abs2():
-    assert abs(-42) == -42, "Should be absolute value of a number"
-
+import unittest
+# Создать класс, который должен наследоваться от класса TestCase
+class TestAbs(unittest.TestCase):
+	# Превратить тестовые функции в методы, добавив ссылку на экземпляр класса self в качестве первого аргумента функции
+    def test_abs1(self):
+    	# Изменить assert на self.assertEqual()
+        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+        
+    def test_abs2(self):
+        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+        
 if __name__ == "__main__":
-    test_abs1()
-    test_abs2()
-    print("Everything passed")
+	#Заменить строку запуска программы
+    unittest.main()
