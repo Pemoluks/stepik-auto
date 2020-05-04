60a84f188da5caa928c0575842a8e1473656a36c
Pemoluks <61008543+Pemoluks@users.noreply.github.com>
2020-05-04 14:03:11 +0300

    Create unittestFinal.py

---
+++ unittestFinal.py
@@ -0,0 +1,118 @@
+from selenium import webdriver
+import time
+import unittest
+# Создать класс, который должен наследоваться от класса TestCase
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_css_selector(".first_block .first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_css_selector(".first_block .second")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_css_selector(".first_block .third")
+        input3.send_keys("sd@mail.ru")
+        # или
+        #elements = browser.find_elements_by_css_selector('.first_block .form-control')
+        #for i in elements:
+        #    i.send_keys('Some text')
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, 'Error welcome text')
+
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+    def test_abs2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_css_selector(".first_block .first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_css_selector(".first_block .second")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_css_selector(".first_block .third")
+        input3.send_keys("sd@mail.ru")
+        # или
+        #elements = browser.find_elements_by_css_selector('.first_block .form-control')
+        #for i in elements:
+        #    i.send_keys('Some text')
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, 'Error welcome text')
+
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+''' красивое решение !!!!
+from selenium import webdriver
+import time
+import unittest
+
+def link_t(link):
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    browser.find_element_by_css_selector(".first_block .first").send_keys("Иван")
+    browser.find_element_by_css_selector(".first_block .second").send_keys("Иванов")
+    browser.find_element_by_css_selector(".first_block .third").send_keys("ivan@yandex.ru")
+    browser.find_element_by_css_selector("button.btn").click()
+
+    time.sleep(1)
+    return browser.find_element_by_tag_name("h1").text
+
+
+class TestReg(unittest.TestCase):
+    def test_reg1(self):
+        self.assertEqual(link_t("http://suninjuly.github.io/registration1.html"),
+                         "Congratulations! You have successfully registered!", "registration is failed")
+
+
+    def test_reg2(self):
+        self.assertEqual(link_t("http://suninjuly.github.io/registration2.html"),
+                         "Congratulations! You have successfully registered!", "registration is failed")
+
+if __name__ == "__main__":
+    unittest.main()
+'''
