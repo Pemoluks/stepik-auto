533a33e6c9eb2283eb6ccaebcfac06628baf0520
Pemoluks <61008543+Pemoluks@users.noreply.github.com>
2020-05-11 13:39:15 +0300

    PYTest. XFail (XPASS ожидаемо падающий тест испрвленный)

---
+++ test_fixture10b.py
@@ -0,0 +1,36 @@
+# Добавим маркировку @pytest.mark.xfail для падающего теста,  который проверяет 
+# наличие кнопки "Избранное"
+# можно добавлять параметр reason. Чтобы увидеть это сообщение в консоли
+# XPASS-тесты (меняем селектор в последнем тесте, чтобы тест начал проходить)
+# pytest -rX -v test_fixture10b.py
+
+
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1():
+
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail(reason="fixing this bug right now")
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("input.btn.btn-default")
